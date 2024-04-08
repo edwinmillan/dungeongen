a4d75629260e7710abacfed6cf5acad161c7e395
Edwin Millan <bassgs3000@gmail.com>
2024-04-08 10:47:47 -0500

    Added some Fields and Optional Typing

--- dungeongen/gate.py
+++ dungeongen/gate.py
@@ -1,16 +1,17 @@
 import random
 import asyncio
+from typing import Optional, Union
 from compendium import Compendium
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, Field
 
 
 class Gate(BaseModel):
-    monster_crs: list[int | float] | None = []
-    boss_crs: list[int | float] | None = []
-    wave_die: int | None = 0
-    wave_size_die: int | None = 0
+    monster_crs: Optional[list[Union[int, float]]] = []
+    boss_crs: Optional[list[Union[int, float]]] = []
+    wave_die: Optional[int] = None
+    wave_size_die: Optional[int] = None
     rank: str
-    compendium: Compendium = Compendium()
+    compendium: Compendium = Field(default=Compendium())
 
     @field_validator("rank")
     def validate_rank(cls, value):
@@ -56,60 +57,60 @@ class Gate(BaseModel):
 
 
 class ERankGate(Gate):
-    monster_crs: list[int | float] = [0, 0.25]
-    boss_crs: list[int | float] = [1, 2]
-    wave_die: int = 6
-    wave_size_die: int = 12
+    monster_crs: list[int | float] = Field(default=[0, 0.25])
+    boss_crs: list[int | float] = Field(default=[1, 2])
+    wave_die: int = Field(default=6)
+    wave_size_die: int = Field(default=12)
 
     def __init__(self):
         super().__init__(rank="E")
 
 
 class DRankGate(Gate):
-    monster_crs: list[int | float] = [0.25, 0.5]
-    boss_crs: list[int | float] = [2, 3]
-    wave_die: int = 8
-    wave_size_die: int = 16
+    monster_crs: list[int | float] = Field(default=[0.25, 0.5])
+    boss_crs: list[int | float] = Field(default=[2, 3])
+    wave_die: int = Field(default=8)
+    wave_size_die: int = Field(default=16)
 
     def __init__(self):
         super().__init__(rank="D")
 
 
 class CRankGate(Gate):
-    monster_crs: list[int | float] = [1, 2]
-    boss_crs: list[int | float] = [3, 4]
-    wave_die: int = 10
-    wave_size_die: int = 20
+    monster_crs: list[int | float] = Field(default=[1, 2])
+    boss_crs: list[int | float] = Field(default=[3, 4])
+    wave_die: int = Field(default=10)
+    wave_size_die: int = Field(default=20)
 
     def __init__(self):
         super().__init__(rank="C")
 
 
 class BRankGate(Gate):
-    monster_crs: list[int | float] = [2, 3]
-    boss_crs: list[int | float] = [4, 5]
-    wave_die: int = 12
-    wave_size_die: int = 24
+    monster_crs: list[int | float] = Field(default=[2, 3])
+    boss_crs: list[int | float] = Field(default=[4, 5])
+    wave_die: int = Field(default=12)
+    wave_size_die: int = Field(default=24)
 
     def __init__(self):
         super().__init__(rank="B")
 
 
 class ARankGate(Gate):
-    monster_crs: list[int | float] = [3, 4]
-    boss_crs: list[int | float] = [5, 6]
-    wave_die: int = 14
-    wave_size_die: int = 28
+    monster_crs: list[int | float] = Field(default=[3, 4])
+    boss_crs: list[int | float] = Field(default=[5, 6])
+    wave_die: int = Field(default=14)
+    wave_size_die: int = Field(default=28)
 
     def __init__(self):
         super().__init__(rank="A")
 
 
 class SRankGate(Gate):
-    monster_crs: list[int | float] = [4, 5]
-    boss_crs: list[int | float] = list(range(7, 25))
-    wave_die: int = 40
-    wave_size_die: int = 100
+    monster_crs: list[int | float] = Field(default=[4, 5])
+    boss_crs: list[int | float] = Field(default=list(range(7, 25)))
+    wave_die: int = Field(default=40)
+    wave_size_die: int = Field(default=100)
 
     def __init__(self):
         super().__init__(rank="S")
