01483fd7fbd5375a6bcee608a1558981720677ba
Edwin Millan <bassgs3000@gmail.com>
2024-04-08 11:41:35 -0500

    Added a little error handling

--- dungeongen/compendium.py
+++ dungeongen/compendium.py
@@ -1,17 +1,21 @@
 import httpx
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, Field, HttpUrl
+from schema.dndapi_model import Monster, MonsterResults, Results
 from yarl import URL
-from schema.dndapi_model import MonsterResults, Results, Monster
 
 
 class Compendium(BaseModel):
     base_url: HttpUrl = Field(default="https://www.dnd5eapi.co/api")
 
     async def _call_api(self, url: URL) -> dict:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(str(url))
-        response.raise_for_status()
-        return response.json()
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(str(url))
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            print(e)
+            return {}
 
     async def get_monsters(self, cr: int | None = None) -> list[Results]:
         url = URL(str(self.base_url)) / "monsters"
