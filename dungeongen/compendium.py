624f40f8b8b158107a6d4492dc0b6e60b7ccd816
Edwin Millan <bassgs3000@gmail.com>
2024-04-08 11:03:24 -0500

    Homogenized base_url

--- dungeongen/compendium.py
+++ dungeongen/compendium.py
@@ -1,11 +1,11 @@
 import httpx
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, Field
 from yarl import URL
 from schema.dndapi_model import MonsterResults, Results, Monster
 
 
 class Compendium(BaseModel):
-    base_url: HttpUrl = "https://www.dnd5eapi.co/api"
+    base_url: HttpUrl = Field(default="https://www.dnd5eapi.co/api")
 
     async def _call_api(self, url: URL) -> dict:
         async with httpx.AsyncClient() as client:
@@ -14,7 +14,7 @@ class Compendium(BaseModel):
         return response.json()
 
     async def get_monsters(self, cr: int | None = None) -> list[Results]:
-        url = URL(self.base_url) / "monsters"
+        url = URL(str(self.base_url)) / "monsters"
         if cr:
             url = url.with_query(challenge_rating=cr)
         result = await self._call_api(url)
@@ -22,7 +22,7 @@ class Compendium(BaseModel):
         return monsters.results
 
     async def get_monster(self, index: str) -> Monster:
-        url = URL(self.base_url) / "monsters" / index
+        url = URL(str(self.base_url)) / "monsters" / index
         result = await self._call_api(url)
         monster = result
         return monster
