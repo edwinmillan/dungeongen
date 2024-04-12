718046797de46a997ce6f0843f2023a7d9d45a14
Edwin Millan <bassgs3000@gmail.com>
2024-04-11 22:55:12 -0500

    Cleaned up and added a little error handling

--- src/chat.py
+++ src/chat.py
@@ -1,8 +1,10 @@
-import httpx
 import json
-from dotenv import load_dotenv
 from os import getenv
-from schema.openrouter_model import Response
+
+import httpx
+from dotenv import load_dotenv
+
+from src.schema.openrouter_model import Response
 
 load_dotenv()
 
@@ -21,16 +23,11 @@ async def send_prompt(prompt: str) -> str:
         async with httpx.AsyncClient() as client:
             response = await client.post(url, headers=headers, data=data)
             response.raise_for_status()
-            chat_response = Response(**response.json())
+            if response.status_code in range(200, 299):
+                chat_response = Response(**response.json())
+                return chat_response.choices[0].message.content
+            else:
+                return {}
     except httpx.HTTPError as e:
         print(f"Error: {e}")
         return {}
-    return chat_response.choices[0].message.content
-
-
-if __name__ == "__main__":
-    import asyncio
-
-    prompt = "What is the meaning of life?"
-    response = asyncio.run(send_prompt(prompt))
-    print(response)
